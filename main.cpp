a5774b2fb02dce6b9cf86668b3456f7bbedb1a2e
dragomirclaudiu <dragomir.clau@gmail.com>
2017-05-29 10:14:53 +0300

    Feature/Change another variable in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <functional>
 int main()
 {
-	std::array<int, 5> arr = {3, 4, 1, 5, 7, 6};
+	std::array<int, 5> arr = {5, 4, 1, 5, 7, 6};
 	std::sort(std::begin(arr), std::end(arr));
 	std::sort(std::begin(arr), std::end(arr),
 	          std::greater<int>{});
