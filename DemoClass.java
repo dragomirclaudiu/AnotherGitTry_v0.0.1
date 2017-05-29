8be3ef721199c5e932d2fea44d2abd4a772deb8e
dragomirclaudiu <dragomir.clau@gmail.com>
2017-05-29 10:13:19 +0300

    Feature/Detele comments DemoClass

--- DemoClass.java
+++ DemoClass.java
@@ -4,51 +4,38 @@ public class DemoClass
 
     public DemoClass()
     {
-        // assign default value
         x = 0;
     }
 
     public DemoClass(int x)
     {
-        // use this.x to refer to the instance variable x
-        // use x to refer to a local variable x (more specifically,
-        // method parameter x)
         this.x = x;
     }
 
     public DemoClass(DemoClass otherDemo)
     {
-        // copy the value from the otherDemo
         this.x = otherDemo.x;
     }
 
-    // static method (aka class method)
     public static void s1() {
         return;
     }
-    // instance method
     public void i1() {
         return;
     }
 
-    // static calling static OK
-    // static calling instance is a compile-time error
     public static void s2() {
-//        i1();     // compile-time error
-        s1();       // DemoClass.s1
+        s1();    
         return;
     }
 
-    // instance calling static OK
-    // instance calling instance OK
     public void i2() {
-        s1();       // DemoClass.s1();
-        i1();       // this.i1();
+        s1();     
+        i1();    
         return;
     }
 
-    // call various versions of overload() based on their 
-    // list of parameters (aka function signatures)
+
     public void overloadTester() {
         System.out.println("overloadTester:\n");
 
