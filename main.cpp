045fcb2cbbe5c36cf2d6fb6c9affc5d2fcefa2e1
dragomirclaudiu <dragomir.clau@gmail.com>
2017-05-29 10:11:32 +0300

    DEV/other small variable change

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <functional>
 int main()
 {
-	std::array<int, 5> arr = {3, 4, 1, 4, 7, 6};
+	std::array<int, 5> arr = {10, 5, 7, 6, 3, 4, 1, 4, 7, 6};
 	std::sort(std::begin(arr), std::end(arr));
 	std::sort(std::begin(arr), std::end(arr),
 	          std::greater<int>{});
